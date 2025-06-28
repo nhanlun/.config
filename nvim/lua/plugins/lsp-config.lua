d453a1265b0afade16e750890ddd1efed244cbd7
Nhan Nguyen <ngthanhnhanlhp2000@gmail.com>
2025-06-28 22:47:07 +0700

    complete nvim config

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,40 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				cmd = {
+					"clangd",
+					"--function-arg-placeholders",
+				},
+				capabilities = capabilities,
+			})
+
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>e", function()
+				vim.diagnostic.open_float(0, { scope = "line" })
+			end, {})
+		end,
+	},
+}
