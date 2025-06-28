35b30160b865efab0d5fb53f7e7071d52d46fe12
Nhan Nguyen <ngthanhnhanlhp2000@gmail.com>
2025-06-28 21:30:39 +0700

    begin nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,37 @@
+------------------------- basics ----------------------------
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.cmd("set smarttab")
+vim.cmd("set clipboard=unnamedplus")
+
+vim.g.mapleader = " "
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	command = "set nornu nonu",
+})
+vim.api.nvim_create_autocmd("BufEnter", {
+	command = "set rnu nu",
+})
+
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+------------------------- lazy ------------------------------
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
